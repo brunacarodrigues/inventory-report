bb10f51027e3b0ed1b0b972ef4ccde60baf8a853
Bruna Rodrigues <brunacris.rodrigues@gmail.com>
2023-11-03 13:08:16 -0300

    task 05 ok

--- inventory_report/inventory.py
+++ inventory_report/inventory.py
@@ -1,2 +1,15 @@
+from inventory_report.product import Product
+from typing import Optional, List
+
+
 class Inventory:
-    pass
+    def __init__(self, data: Optional[List[Product]] = None):
+        self._data = data or []
+
+    def add_data(self, data: List[Product]) -> None:
+        for product in data:
+            self._data.append(product)
+
+    @property
+    def data(self) -> List[Product]:
+        return self._data
