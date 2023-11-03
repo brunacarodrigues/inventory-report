887f190cc0b8b3f61195f18ba7791db34d6a5bbe
Bruna Rodrigues <brunacris.rodrigues@gmail.com>
2023-11-03 12:40:07 -0300

    task 02 ok

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,2 +1,22 @@
+from inventory_report.product import Product
+
+
 def test_product_report() -> None:
-    raise NotImplementedError
+    product = {
+        "id": "1",
+        "company_name": "toys",
+        "product_name": "rubber ducky",
+        "manufacturing_date": "2020-09-05",
+        "expiration_date": "2023-09-05",
+        "serial_number": "159753",
+        "storage_instructions": "dry after use",
+    }
+
+    instance_product = Product(**product)
+
+    assert str(instance_product) == (
+        "The product 1 - rubber ducky with serial number 159753 "
+        "manufactured on 2020-09-05 by the company toys valid "
+        "until 2023-09-05 must be stored according to the "
+        "following instructions: dry after use."
+    )
